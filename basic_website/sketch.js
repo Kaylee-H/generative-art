00479972358467f2ee6e06c9bd71dea4c4a85c53
Kaylee <78274513+Kaylee-H@users.noreply.github.com>
2021-04-27 12:25:11 +0200

    test

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -1,14 +1,37 @@
 /* 👇 Start writing your p5.js code here */
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0);
-  ellipse(width / 2, height / 2, 50, 50);
-}
+  background(0, 100, 0, 1);
+  
+  orbitControl();
+  
+  push();
+  blendMode(SCREEN);
+  stroke(mouseX, 100, 100, 0.1);
+  noFill();
+  // noStroke();
+  for (let i = 0; i < 10; i++) {
+    // // strokeCap(ROUND);
+    // strokeJoin(ROUND);
+    // strokeWeight(10);
+    box(100 - i * 1);
+  }
+  fill(mouseX + 180, 100, 100, 0.1);
+  // stroke(0);
+  noStroke();
+  box(100);
+  pop();
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+
+
+// function draw() {
+//   background(0);
+//   ellipse(width / 2, height / 2, 50, 50);
+// }
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
