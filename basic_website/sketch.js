80d4c522aa3f2d17e080fe733043dab36d955eb8
Kaylee <78274513+Kaylee-H@users.noreply.github.com>
2021-04-28 12:55:01 +0200

    Rect

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -1,37 +1,63 @@
 /* 👇 Start writing your p5.js code here */
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0, 100, 0, 1);
-  
-  orbitControl();
-  
-  push();
-  blendMode(SCREEN);
-  stroke(mouseX, 100, 100, 0.1);
-  noFill();
-  // noStroke();
-  for (let i = 0; i < 10; i++) {
-    // // strokeCap(ROUND);
-    // strokeJoin(ROUND);
-    // strokeWeight(10);
-    box(100 - i * 1);
-  }
-  fill(mouseX + 180, 100, 100, 0.1);
-  // stroke(0);
-  noStroke();
-  box(100);
-  pop();
+  background(200);
+  Rect (witdh / 2, height / 2, 50, 50);
+
+}
 
 
+
+
+// let rad = 50; // Width of the shape
+// let xpos, ypos; // Starting position of shape
+
+// let xspeed = 1; // Speed of the shape
+// let yspeed = 1; // Speed of the shape
+
+// let xdirection = 5; // Left or Right
+// let ydirection = 6; // Top to Bottom
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   colorMode(HSB);
+//   frameRate(50);
+//   ellipseMode(RADIUS);
+//   // Set the starting position of the shape
+//   xpos = width / 7;
+//   ypos = height / 7;
+//   stroke(255)
+//   strokeWeight(5);
+//   noFill();
+// }
 
 // function draw() {
 //   background(0);
-//   ellipse(width / 2, height / 2, 50, 50);
+
+//   // Update the position of the shape
+//   xpos = xpos + xspeed * xdirection;
+//   ypos = ypos + yspeed * ydirection;
+
+//   // Test to see if the shape exceeds the boundaries of the screen
+//   // If it does, reverse its direction by multiplying by -1
+//   if (xpos >= width - rad || xpos <= rad) {
+//     xdirection *= -1;
+//     stroke(random(5, 245), 255, 255);
+//   }
+//   if (ypos >= height - rad || ypos <= rad) {
+//     ydirection *= -1;
+//     stroke(random(5, 245), 255, 255);
+//   }
+
+//   // Draw the shape
+//   ellipse(xpos, ypos, rad, rad);
+
 // }
 
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
