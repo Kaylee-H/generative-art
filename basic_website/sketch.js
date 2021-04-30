268582326cafd514020a1a4ec4af22661f50954c
Kaylee <78274513+Kaylee-H@users.noreply.github.com>
2021-04-30 13:12:33 +0200

    Options

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -12,65 +12,37 @@
 //   rect(windowWidth / 2, windowHeight /2, 300, 300);
 // }
 
-let isOn1 = false;
-let hasLeft1 = false;
-
-function setup() {
+function setup() { 
   createCanvas(windowWidth, windowHeight);
-}
 
-function draw() {
-  //block1:
-        push();
-        fill(0);
-        stroke(255);
-        rect(0,0, windowWidth/3, windowHeight);
-      	pop();
-        
-        //block2:
-        push();
-        fill(0);
-        stroke(255);
-        rect(windowWidth/3,0, windowWidth/3, windowHeight);
-        pop();
-        
-        //block3:
-        push();
-        fill(0);
-        stroke(255);
-        rect(windowWidth*2, 0, windowWidth/3, windowHeight);
-        pop();
-  
-  fill(100, 0, 100);
-  stroke(255);
+} 
+
+function draw() { 
   background(0);
+  
+  // colorblock + mouse
+  if (mouseX < width/2) {
+    fill(0);
+    stroke(255,0,0);
+    strokeWeight(5);
+    rect(0, 0, width/2, height);
+  } else if (mouseX <= width*2) {
+    fill(0);
+    stroke(0,0,255);
+    strokeWeight(5);
+    rect(width/2, 0, width/2, height);
+  } 
 
-  if (mouseX < width / 3 ) {
-    if(hasLeft1) {
-      isOn1 = !isOn1;
-      hasLeft1 = false;
-    }
-
-  } else if (mouseX < width / 3) {
-    hasLeft1 = false;
-    rect(0, 0, width / 3, height);
-
-  }  else if (mouseX <= width * 2 / 3) {
-    hasLeft1 = false;
-    rect(width / 3, 0, width / 3, height);
-
-  } else {
-    hasLeft1 = false;
-    rect(width * 2 / 3, 0, width / 3, height);
-  }
+  fill(255);
+  stroke(0);
+  ellipse(970,290,200,200);
+  square(230,190,200);
 
 }
 
-  
- 
 
 
-
+/* 3D-box*/
 //   let locX = mouseX - height / 2;
 //   let locY = mouseY - width / 2;
 
