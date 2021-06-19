7e27e320cdf7d611ecb841e9381f2a1ce1658343
Kaylee <78274513+Kaylee-H@users.noreply.github.com>
2021-06-19 11:26:56 +0200

    Add HTML+JS

--- basic_website/scene2.js
+++ basic_website/scene2.js
@@ -1,32 +1,41 @@
-var scene1 = true;
-var scene2 = false;
-var scene3 = false;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
    angleMode(DEGREES);
+   fading = false;
 }
 
 function draw() {
   background(0, 30, 40);
-  
+
     push();
+    translate(0,25);
     fill(0,0,255);
     rotateX(frameCount * 2);
     rotateY(frameCount * 2);
-    box(150);
+    box(140);
+    pop();
     
+    push();
+    translate(-100,25);
     fill(255,0,0);
     rotateX(frameCount * 2);
     rotateY(frameCount * 2);
-    box(150);
+    box(140);
+    pop();
  
+    push();
+    translate(100,25);
     fill(100,100,100);
     rotateX(frameCount * 2);
     rotateY(frameCount * 2);
-    box(150);
+    box(140);
     pop();
+
 }
+
+
 
 
 
